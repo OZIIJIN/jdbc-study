cc48cd058b602598025b246d3805b2439508f72e
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-05 12:21:46 +0900

    JDBC 삽입/조회 실습

--- src/test/java/JDBCTest.java
+++ src/test/java/JDBCTest.java
@@ -2,11 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class JDBCTest {
 
 	@Test
+	@DisplayName("JDBC 테이블 생성 실습")
 	void jdbcTest() throws SQLException {
 		//given
 
@@ -30,7 +32,7 @@ public class JDBCTest {
 			String creatSql = "CREATE TABLE ACCOUNT (id SERIAL PRIMARY KEY, username varchar(255), password varchar(255))";
 			// 쿼리 실행
 			PreparedStatement statement = connection.prepareStatement(creatSql);
-			statement.execute();
+			statement.execute(); //execute -> 쿼리를 날림
 
 			// 제일 중요한 자원 해제
 			statement.close();
@@ -41,6 +43,35 @@ public class JDBCTest {
 
 		//then
 		//DB 확인
+	}
+	@Test
+	@DisplayName("JDBC 삽입/조회 실습")
+	void jdbcInsertSelectTest() throws SQLException {
+		// given
+		String url = "jdbc:postgresql://localhost:5432/messenger";
+		String username = "yejin";
+		String password = "pass";
+
+		// when
+		//자동으로 close(자원해제) 하는 방법 : try() -> 괄호 안에 connection 넣어주기
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			System.out.println("Connection created: " + connection);
+
+			String insertSql = "INSERT INTO ACCOUNT (id, username, password) VALUES ((SELECT coalesce(MAX(ID), 0) + 1 FROM ACCOUNT A), 'user1', 'pass1')";
+			try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
+				statement.execute();
+			}
+
+			// then
+			String selectSql = "SELECT * FROM ACCOUNT";
+			try (PreparedStatement statement = connection.prepareStatement(selectSql)) {
+				var rs = statement.executeQuery();
+				while (rs.next()) {
+					System.out.printf("%d, %s, %s", rs.getInt("id"), rs.getString("username"),
+						rs.getString("password"));
+				}
+			}
+		}
 	}
 
 }
